181b464a46c936737e7df1c06b9e314e2f9bc636
jalil <jalil@1001.digital>
2026-02-12 10:27:53 +0100

    Find artifacts from root

---
+++ packages/ui/app/components/FindArtifact.vue
@@ -0,0 +1,30 @@
+<template>
+  <Form @submit.prevent="go">
+    <FormLabel label="Contract address">
+      <FormItem>
+        <input
+          v-model="contract"
+          placeholder="0x..."
+          required
+        />
+      </FormItem>
+    </FormLabel>
+    <FormLabel label="Token ID">
+      <FormItem>
+        <input
+          v-model="token"
+          placeholder="1"
+          required
+        />
+      </FormItem>
+    </FormLabel>
+    <Button type="submit">View</Button>
+  </Form>
+</template>
+
+<script setup lang="ts">
+const contract = ref('')
+const token = ref('')
+
+const go = () => navigateTo(`/${contract.value}/${token.value}`)
+</script>
---
+++ packages/web/app/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <main>
+    <Card>
+      <header>
+        <h1>Find Artifact</h1>
+      </header>
+      <FindArtifact />
+    </Card>
+  </main>
+</template>
+
+<style scoped>
+main {
+  max-width: 32rem;
+  margin: var(--spacer) auto;
+  padding: var(--spacer);
+}
+
+.card {
+  header {
+    padding-bottom: var(--spacer);
+    border-bottom: var(--border);
+  }
+}
+</style>
