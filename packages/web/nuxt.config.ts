eb74c53291df44333987fab4b29eac3bd9a1b55e
jalil <jalil@1001.digital>
2026-02-16 19:14:11 +0100

    Set custom font

--- packages/web/nuxt.config.ts
+++ packages/web/nuxt.config.ts
@@ -1,6 +1,12 @@
+import { fileURLToPath } from 'node:url'
+import { dirname, join } from 'node:path'
+
+const currentDir = dirname(fileURLToPath(import.meta.url))
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   extends: ['../ui'],
   devtools: { enabled: true },
   ssr: false,
+  css: [join(currentDir, './app/assets/styles/index.css')],
 })
