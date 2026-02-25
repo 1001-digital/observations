137ca87f3135e9681c53e1163b96d8dda7c23bbf
jalil <jalil@1001.digital>
2026-02-25 16:57:46 +0100

    Link to block explorer for profile links

--- packages/ui/lib/src/AppLink.vue
+++ packages/ui/lib/src/AppLink.vue
@@ -1,30 +1,39 @@
 <template>
   <a
-    v-if="isExternal"
-    :href="(to as string)"
-    :target="target"
+    v-if="href"
+    :href="href"
+    :target="target ?? (isExternal ? '_blank' : undefined)"
   >
     <slot />
   </a>
-  <RouterLink
-    v-else
-    :to="to!"
-    :target="target"
-  >
+  <span v-else>
     <slot />
-  </RouterLink>
+  </span>
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue'
-import { RouterLink } from 'vue-router'
+import { useBlockExplorer } from '@1001-digital/components'
 
 const props = defineProps<{
   to?: string | Record<string, any>
   target?: string
 }>()
 
+const blockExplorer = useBlockExplorer()
+
 const isExternal = computed(
   () => typeof props.to === 'string' && /^https?:\/\//.test(props.to),
 )
+
+const href = computed(() => {
+  if (typeof props.to !== 'string') return null
+  if (isExternal.value) return props.to
+
+  // Resolve internal address paths to block explorer
+  const match = props.to.match(/^\/(?:observer\/)?(0x[a-fA-F0-9]{40})$/)
+  if (match) return `${blockExplorer}/address/${match[1]}`
+
+  return null
+})
 </script>
