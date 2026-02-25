80129237b4b606d61f764263698bfa3ba3ff182f
jalil <jalil@1001.digital>
2026-02-25 18:01:47 +0100

    Add image types

---
+++ packages/ui/components/src/env.d.ts
@@ -0,0 +1,15 @@
+declare module '*.svg' {
+  const src: string
+  export default src
+}
+
+declare module '*.png' {
+  const src: string
+  export default src
+}
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
