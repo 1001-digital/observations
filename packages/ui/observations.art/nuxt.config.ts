4a830ec03fb9227989ac764a268fbf75e0de5c3f
jalil <jalil@1001.digital>
2026-03-18 12:18:49 +0100

    Fix hot reload
    Fix

--- packages/ui/observations.art/nuxt.config.ts
+++ packages/ui/observations.art/nuxt.config.ts
@@ -6,4 +6,5 @@ const currentDir = dirname(fileURLToPath(import.meta.url))
 export default defineNuxtConfig({
   extends: ['../app'],
   css: [join(currentDir, './app/assets/styles/index.css')],
+  watch: ['../components/src/**', '../app/app/**'],
 })
