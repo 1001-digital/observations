1e32957b7f496f29ad9f04733885272d94d52331
jalil <jalil@1001.digital>
2026-03-18 15:35:51 +0100

    Nicer bg color

--- packages/ui/components/src/components/TokenDetail.vue
+++ packages/ui/components/src/components/TokenDetail.vue
@@ -248,7 +248,7 @@ useBreadcrumb(
   container-type: size;
   border-bottom: var(--border);
   height: 100cqw;
-  background: var(--gray-z-4);
+  background: var(--gray-z-3);
 
   .artifact-visual {
     width: min(80cqw, 80cqh);
