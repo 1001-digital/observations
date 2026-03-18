d01c1d59cdbb402c450526143d4d18c2cc7a1124
jalil <jalil@1001.digital>
2026-03-18 11:53:40 +0100

    Refactor to clean minimal base app and observations.art styled app

---
+++ packages/ui/observations.art/nuxt.config.ts
@@ -0,0 +1,9 @@
+import { fileURLToPath } from 'node:url'
+import { dirname, join } from 'node:path'
+
+const currentDir = dirname(fileURLToPath(import.meta.url))
+
+export default defineNuxtConfig({
+  extends: ['../app'],
+  css: [join(currentDir, './app/assets/styles/index.css')],
+})
